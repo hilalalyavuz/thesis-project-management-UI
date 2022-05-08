23237fe0de63f27ba7f87ff479d72479b8d30ee8
Görkem Deniz SEYHAN <69456932+Denowen@users.noreply.github.com>
2022-05-08 22:11:04 +0300

    SignIn

--- src/views/authorize/SignIn.js
+++ src/views/authorize/SignIn.js
@@ -15,15 +15,64 @@ import TextareaAutosize from '@mui/material/TextareaAutosize';
 import singin from '../../img/signin.svg';
 import logo from '../../img/logo.png';
 import wave from '../../img/wave.svg';
+import axios from 'axios';
+import { createBrowserHistory } from 'history';
 
 
 
 
 export default function SignIn() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const toast = useRef(null);
+    const [token,setToken] = useState("token");
+    const [role,setRole] = useState("role");
     
+    const send = () =>{
+
+        if(!email.includes('@')){
+            toast.current.show({severity:'warn', detail:"Please enter a part following '@'.", life: 3000});
+        }
+        
+        const GetData = async () => { 
+            await axios.post('https://localhost:7084/Auth/login',
+            {
+                "email": email,
+                "password": password
+              }).then(response => {
+                toast.current.show({severity:'success', summary: 'Success Message', detail:'Message Content', life: 3000});
+                setToken(response.data[0])
+                sessionStorage.setItem("token", response.data[0]);
+                setRole(response.data[1])
+                sessionStorage.setItem("role", response.data[1]);
+                sessionStorage.setItem("email",email);
+                
+                if(response.data[1] == "student"){
+                    createBrowserHistory().push('/Home');
+                    window.location.reload();
+                }else if(response.data[1] == "supervisor"){
+                    createBrowserHistory().push('/SupervisorHome');
+                    window.location.reload();
+                }else if(response.data[1] == "admin"){
+                    createBrowserHistory().push('/DocumentsAdmin');
+                    window.location.reload();
+                }
+                
+            }).catch(error => {
+                toast.current.show({severity:'error', summary: 'Failed to login', life: 3000});
+            });;  
+            
+            };
+            GetData();
+
+    }
+
+
     return(
         <>
         <div className='Page'>
+        <Toast ref={toast} />
 
 
 <div className='Main'>
@@ -49,20 +98,21 @@ export default function SignIn() {
             </Card>
             <Card style={{display:'flex',flex:'1', boxShadow:'rgb(204 204 204) 0rem 0rem 2rem 7px'}}>
                 <div style={{display:'flex',flex:'1', justifyContent:'center'}}>
-                        <form style={{display:'flex', flexDirection:'column', justifyContent:'center'}}>
+                        <div style={{display:'flex', flexDirection:'column', justifyContent:'center'}}>
                             <h2 style={{display:'flex',justifyContent:'center'}}>Welcome</h2>
-                            <TextField id="outlined-basic" label="Id" variant="outlined" />
+                            <TextField id="email" label="Email" autoComplete="email" type="email" variant="outlined" value={email} onChange={(e)=>setEmail(e.target.value)}/>
                             <TextField
             id="outlined-password-input"
             label="Password"
             type="password"
             autoComplete="current-password"
+            value={password} onChange={(e)=> setPassword(e.target.value)}
             />
             
-                <input style={{marginTop:'2rem',height:'2rem',borderRadius:'1rem',border:'1px solid green', backgroundColor:'green', color:'white', cursor:'pointer'}} type="submit" class="btn" value="GİRİŞ"/>
+                <Button style={{marginTop:'2rem',height:'2rem',borderRadius:'1rem',border:'1px solid #50C878', backgroundColor:'#50C878', color:'white', cursor:'pointer'}} onClick={send}>Sign In</Button>
             
             
-                        </form>
+                        </div>
                     </div>
             </Card>
       </div>
